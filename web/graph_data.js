15fb5aa232a4460d56c0311715561674bde32a11
Duga123vn4 <Duga123vn4@users.noreply.github.com>
2026-08-31 01:25:59 +0700

    Novel AI V3.0: Deploy Unified Studio Tools Dropdown Menu with backup

--- web/graph_data.js
+++ web/graph_data.js
@@ -498,8 +498,8 @@ window.GRAPH_DATA = {
     ],
     "edges": [],
     "factions": [
-      "Khác",
-      "Nhân vật chính"
+      "Nhân vật chính",
+      "Khác"
     ]
   },
   "Cô Bạn Thuở Nhỏ Khăng Khăng Bạn Đời Của Cậu Ấy Là Tôi ―― Hóa Ra Người Phát Điên Lại Chính Là Cô Ấy": {
@@ -771,8 +771,8 @@ window.GRAPH_DATA = {
     ],
     "edges": [],
     "factions": [
-      "Khác",
-      "Nhân vật chính"
+      "Nhân vật chính",
+      "Khác"
     ]
   }
 };
